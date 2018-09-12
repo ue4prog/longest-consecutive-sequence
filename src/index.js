5e6d81c58a4c4f785f646c70b71dcdacdeb1f8a3
ue4prog <vladtet2000@mail.ru>
2018-09-12 23:01:14 +0300

    mmm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  if (array.length === 0) {return 0}
+
+  let unique = new Set()
+
+  let maxSeqLen = 1
+
+  array.forEach((number) => {
+
+      unique.add(number)
+
+  })
+
+  for (let i = 0; i < array.length; i++) {
+
+      if (unique.has(array[i] +1)) {
+
+          let sequence = array[i]+1
+
+          let seqLength = 2
+
+          while (unique.has(sequence+1)) {
+
+              seqLength++
+
+              sequence++
+
+          }
+
+          if (seqLength > maxSeqLen) {maxSeqLen = seqLength}
+
+      }
+
+  }
+
+
+
+  return maxSeqLen
 }
